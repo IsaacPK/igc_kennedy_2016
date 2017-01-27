76dba30f783c4b486af78feaa2b57c38d2792c93
lijasmine01 <lijasmine01@gmail.com>
2017-01-27 14:41:47 -0600

    Update Example.java
    Jan 27

--- music-maker/musicgen/Example.java
+++ music-maker/musicgen/Example.java
@@ -6,6 +6,11 @@ import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Synthesizer;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.TimeUnit;
 import java.util.Random;
 
@@ -33,23 +38,49 @@ public class Example {
 		
 		int counter = 0;
 		
-		while (counter < 20){
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("musicgen.txt", "UTF-8");
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			
+			return;
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			
+			return;
+		}
+		   
 		
-		int myNote = generateOptions (60,myRandom);
-		playNote (mc, myNote );
+		while (counter < 10){
+			
 		
-		myRandom = getRandom();
+			int myNote = generateOptions (65 ,myRandom);
+			playNote (mc, myNote );
 		
-		System.out.println(myNote);
+			myRandom = getRandom();
 		
-		int myNote2 = generateOptions (myNote, myRandom);
-		playNote (mc, myNote2);
+			System.out.println(myNote);
 		
-		System.out.println(myNote2);
+			System.out.println (myRandom);
 		
-		System.out.println(myRandom);
+			myRandom = getRandom();
+		
+			int myNote2 = generateOptions (myNote, myRandom);
+		
+			playNote (mc, myNote2);
+		
+			System.out.println(myNote2);
+		
+			System.out.println(myRandom);
 	
-		counter++;
+			counter++;
+			
+			writer.println(myNote);
+		    writer.println(myNote2);
+		
 		}
 		
 		
@@ -59,6 +90,8 @@ public class Example {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		writer.close();
 	}
 
 	private static void playNote(MidiChannel[] channel, int noteNum) {
@@ -72,7 +105,7 @@ public class Example {
 			e.printStackTrace();
 		}
 		channel[5].allNotesOff();
-		System.out.println("done");
+	//	System.out.println("done");
 	}
 	
 	
@@ -103,8 +136,8 @@ public class Example {
 		 		return tempOptions [randomNumber];
 		
 		 	}
+	
+
 
 		
-	}
-	
-	
+}
