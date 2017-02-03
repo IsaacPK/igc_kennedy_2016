fc870081ba4d80f904bbb149f52e49d394c0b529
lijasmine01 <lijasmine01@gmail.com>
2017-02-03 15:06:34 -0600

    Update musicreader.java

--- music-maker/musicgen/musicreader.java
+++ music-maker/musicgen/musicreader.java
@@ -1,5 +1,6 @@
 package musicgen;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -14,89 +15,15 @@ import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Synthesizer;
 
-public class Musicreader {
+public class musicreader {
 	
 	public static void main(String[] args) {
 		
-		Synthesizer synth;
-		try {
-			synth = MidiSystem.getSynthesizer();
-			synth.open();
-		} catch (MidiUnavailableException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
-		}
+		PlayWindow app = new PlayWindow();
 		
-		final MidiChannel[] mc = synth.getChannels();
-		Instrument[] instr = synth.getDefaultSoundbank().getInstruments();
-		synth.loadInstrument(instr[90]);
-		
-		
-		
-		 // The name of the file to open.
-        String fileName = "musicgen.txt";
 
-        // This will reference one line at a time
-        String line = null;
-
-        try {
-            // FileReader reads text files in the default encoding.
-            FileReader fileReader = 
-                new FileReader(fileName);
-
-            // Always wrap FileReader in BufferedReader.
-            BufferedReader bufferedReader = 
-                new BufferedReader(fileReader);
-
-            while((line = bufferedReader.readLine()) != null) {
-            	
-            	int result = Integer.parseInt(line);
-            	
-            	playNote(mc,result);
-            }   
-
-            // Always close files.
-            bufferedReader.close();         
-        }
-        catch(FileNotFoundException ex) {
-            System.out.println(
-                "Unable to open file '" + 
-                fileName + "'");                
-        }
-        catch(IOException ex) {
-            System.out.println(
-                "Error reading file '" 
-                + fileName + "'");                  
-            // Or we could just do this: 
-            // ex.printStackTrace();
-        }
-
-		
-		playNote(mc,60);
-		
-		
-		try {
-			TimeUnit.SECONDS.sleep(1);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	
 	}
+
 	
-	private static void playNote(MidiChannel[] channel, int noteNum) {
-		
-		channel[5].noteOn(noteNum, 200);
-		
-		try {
-			TimeUnit.MILLISECONDS.sleep(400);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		channel[5].allNotesOff();
-	//	System.out.println("done");
-	}
+	
 }
