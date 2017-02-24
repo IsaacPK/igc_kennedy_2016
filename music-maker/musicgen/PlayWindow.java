f92636c5001fe78713154f2b922045ba733da671
lijasmine01 <lijasmine01@gmail.com>
2017-02-24 11:19:39 -0600

    Update PlayWindow.java

--- music-maker/musicgen/PlayWindow.java
+++ music-maker/musicgen/PlayWindow.java
@@ -11,8 +11,10 @@ package musicgen;
 import java.awt.*;        // Using AWT container and component classes
 import java.awt.event.*;  // Using AWT event classes and listener interfaces
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
@@ -30,29 +32,25 @@ import java.awt.Color;
 // An AWT program inherits from the top-level container java.awt.Frame
 public class PlayWindow extends Frame implements WindowListener {
    private Label lblCount, lblRecreate;    // Declare a Label component 
-   private TextField tfCount; // Declare a TextField component 
-   private Button btnCount, btnRecreate;   // Declare a Button component
+   private TextField tfCount, tfPlayback; // Declare a TextField component 
+   private Button btnCount, btnRecreate, btnNew, btnfilefind;   // Declare a Button component
    private int count = 0;     // Counter's value
+   private JComboBox box;
    
   JPanel controlPanel= new JPanel();
   private JFrame mainFrame;
  
    // Returns an ImageIcon, or null if the path was invalid. 
    private static ImageIcon createImageIcon(String path,
-      String description) {
+     String description) {
 	   
    
-         return new ImageIcon("maxresdefault.jpg", "cat in toilet roll");
+       return new ImageIcon("maxresdefault.jpg", "cat in toilet roll");
       
    }
-   private void showImageIconDemo(){
-      
-      ImageIcon icon = createImageIcon("/resources/java_icon.png","Java");
-
-      JLabel commentlabel = new JLabel("", icon,JLabel.CENTER);
-      controlPanel.add(commentlabel);
-      mainFrame.setVisible(true);  
-   }
+   
+   private static String [] filename = {"file 1", "file 2"};
+   
    // Constructor to setup GUI components and event handlers
    public PlayWindow () {
 	   
@@ -95,17 +93,42 @@ public class PlayWindow extends Frame implements WindowListener {
        
       Panel Panel2 = new Panel();
       add(Panel2);
-        tfCount = new TextField("0", 10); // construct the TextField component
-     
+      
+      tfCount = new TextField(3); // construct the TextField component
       Panel2.add(tfCount);                     // "super" Frame adds TextField;
+      
  
+      btnNew = new Button ("create new folder");
+      Panel2.add(btnNew);
+      btnNew.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent evt) {
+             newFile();
+          }
+       });
+      
+      box = new JComboBox (filename);
+      Panel2.add(box);
+      
+      btnfilefind = new Button ("find file");
+      Panel2.add(btnfilefind);
+      btnfilefind.addActionListener(new ActionListener() {
+          @Override
+         public void actionPerformed(ActionEvent evt) {
+           findfile();
+         }
+     });
    
-      ImageIcon icon = createImageIcon ("C:/Eclipse/workspace/music-maker/image/maxresdefault.png", "");
+      
+      
+      
+      ImageIcon icon = createImageIcon ("maxresdefault.png", "");
      JLabel label1 = new JLabel("", icon, JLabel.CENTER);
      this.add(label1);
       
+     addWindowListener(this);
      
-      this.setBackground(Color.cyan);
+      this.setBackground(new Color (126,171, 166));
       setTitle("Play Window");  // "super" Frame sets its title
       setSize(300, 400);        // "super" Frame sets its initial window size
    
@@ -119,12 +142,95 @@ public class PlayWindow extends Frame implements WindowListener {
 	// TODO Auto-generated method stub
 	return;
 }
+   
 
 
+    public void findfile () {
+	
+	Synthesizer synth;
+	try {
+		synth = MidiSystem.getSynthesizer();
+		synth.open();
+	} catch (MidiUnavailableException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		return;
+	}
+	
+	final MidiChannel[] mc = synth.getChannels();
+	Instrument[] instr = synth.getDefaultSoundbank().getInstruments();
+	synth.loadInstrument(instr[90]);
+	
+	
+	
+	 // The name of the file to open.
+   String fileName = tfCount.getText();
+
+   // This will reference one line at a time
+   String line = null;
+
+   try {
+       // FileReader reads text files in the default encoding.
+       FileReader fileReader = 
+           new FileReader(fileName);
+
+       // Always wrap FileReader in BufferedReader.
+       BufferedReader bufferedReader = 
+           new BufferedReader(fileReader);
+
+       while((line = bufferedReader.readLine()) != null) {
+       	
+       	int result = Integer.parseInt(line);
+       	
+       	playNote(mc,result);
+       }   
+
+       // Always close files.
+       bufferedReader.close();         
+   }
+   catch(FileNotFoundException ex) {
+       System.out.println(
+           "Unable to open file '" + 
+           fileName + "'");                
+   }
+   catch(IOException ex) {
+       System.out.println(
+           "Error reading file '" 
+           + fileName + "'");                  
+       // Or we could just do this: 
+       // ex.printStackTrace();
+   }
 
 
+	
+	
+	try {
+		TimeUnit.SECONDS.sleep(1);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
 
-public void ReadPlay() {
+
+    public void newFile () {
+	
+	String filename = tfCount.getText();
+     tfCount.setText("");  // Clear input TextField
+
+    try {
+    	BufferedWriter writer = new BufferedWriter (new FileWriter (filename) );
+    	
+    	writer.close();
+    	
+    } catch (IOException e) {
+    	
+    	e.printStackTrace();
+    }
+
+	}
+
+    public void ReadPlay() {
 	   Synthesizer synth;
 		try {
 			synth = MidiSystem.getSynthesizer();
@@ -240,9 +346,9 @@ public void ReadPlay() {
 			return;
 		}
 		
-		int lastnote = 79;
+		int lastnote = 65;
 		
-		while (counter < 10){
+		while (counter < 2){
 			
 		
 			int myNote = generateOptions (lastnote ,myRandom);
@@ -309,7 +415,7 @@ public void ReadPlay() {
 	 	}
 	
 	   @Override
-	   public void windowClosing(WindowEvent evt) {
+	public void windowClosing(WindowEvent evt) {
 	      System.exit(0);  // Terminate the program
 	   }
 	@Override
